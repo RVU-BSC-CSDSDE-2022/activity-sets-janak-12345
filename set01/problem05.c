bbad0275ae1a0ed65705a02daeedb4da73a17ad4
Janak R R <115694395+janak-12345@users.noreply.github.com>
2022-12-21 08:25:28 +0000

    commit

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,21 +1,8 @@
-#include<stdio.h> 
+#include<stdio.h>
+int input() 
+int compare(int a, int b, int c,); 
+void output(int a, int b, int largest); 
 
-void compare(int a, int b, int c); 
 
-int main() 
-{ 
-    int a, b, c; 
-   
-    printf("Enter three numbers: "); 
-    scanf("%d%d%d", &a, &b, &c); 
-   
-    compare(a, b, c); 
-   
-    return 0; 
-} 
 
-void compare(int a, int b, int c) 
-{ 
-    if (a > b && a > c) 
-        printf("%d is the largest number.", a); 
-   
+    
